b25cebaaa91a0d2ac171477da98126a8872ec3a6
Lukas Rahmn <l.rahmn@gmail.com>
2015-05-07 14:47:19 +0200

    Updated test for the removal of core math

--- Test1/tests.h
+++ Test1/tests.h
@@ -3,7 +3,7 @@
 #include "modules/operators.h"
 #include "core/interpreter.h"
 #include "core/corax_virtual_machine.h"
-#include "core/core_math.h"
+
 #include "profiling.h"
 #include "core/export_lib_version.h"
 #include "ptr_protect.h"
