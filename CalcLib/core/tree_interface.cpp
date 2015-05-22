24f14e9bdd7c36f401d6285dc5b63a69619c2076
Lukas Rahmn <l.rahmn@gmail.com>
2015-05-23 01:51:52 +0200

    Further linux fixes

---
+++ CalcLib/core/tree_interface.cpp
@@ -0,0 +1,6 @@
+#include "core/tree_interface.h"
+
+tree::node_base::~node_base()
+{
+	
+}
--- CalcLib/plugins/plugin_loader.cpp
+++ CalcLib/plugins/plugin_loader.cpp
@@ -4,6 +4,13 @@
 #include <Windows.h>
 #endif //WINDOWS
 
+#ifdef LINUX
+#include <dlfcn.h>
+#include <sys/types.h>
+#include <dirent.h>
+#endif
+
+
 plugin::pluginManagerOops::pluginManagerOops(std::string inf)
 {
 	this->_isCritical = true;
@@ -139,6 +146,89 @@ void windows_plugin_manager::load_dll(windows_plugin_manager::plugin_name_contai
  }
 
 #endif //WINDOWS
+
+#ifdef LINUX
+class linux_plugin_manager : public plugin::plugin_manager
+ {
+	 //Windows specific functions
+	 typedef void* Lib;
+	 typedef std::string plugin_name_container;
+	 std::vector<Lib> libs;
+	 std::vector<plugin_name_container> plugin_names;
+	 void get_plugin_names();
+	 void load_lib(plugin_name_container container);
+	 void unload_all_lib();
+ protected:
+	 void nativeLoadPlugin();
+ public:
+	 virtual ~linux_plugin_manager();
+
+ };
+	
+	void linux_plugin_manager::get_plugin_names()
+	{
+		DIR *dp;
+    	struct dirent *dirp;
+    	if((dp  = opendir("./plugins/")) == NULL)
+		{
+        	
+		 throw plugin::pluginManagerOops("Failed to search folders");
+    	}
+
+    	while ((dirp = readdir(dp)) != NULL) 
+		{
+        	plugin_names.push_back(std::string(dirp->d_name));
+    	}
+    	closedir(dp);
+   
+		
+	}
+	
+	void linux_plugin_manager::load_lib(plugin_name_container container)
+	{
+		 Lib newLib = dlopen( std::string(std::string(".//plugins//") + container).c_str(),RTLD_NOW);
+	 if (newLib == NULL)
+	 {
+		
+		 throw plugin::pluginManagerOops(std::string(dlerror()));
+	 }
+	 else
+	 {
+		 libs.push_back(newLib);
+	 }
+	 plugin::plugin_constructor my_constr = (plugin::plugin_constructor)dlsym(newLib, "PLUGIN_ENTRY");
+	 if (!my_constr)
+	 {
+		 throw plugin::pluginManagerOops("Failed to find plugin entry point");
+	 }
+	 else
+	 {
+		 auto tmp = my_constr();
+		 this->loaded_plugins.push_back(tmp);
+	}	
+	}
+	
+	void  linux_plugin_manager::unload_all_lib()
+	{
+		for(Lib current_lib : libs)
+		{
+			dlclose(current_lib);
+		}
+	}
+ void  linux_plugin_manager::nativeLoadPlugin()
+ {
+	 get_plugin_names();
+	 for(linux_plugin_manager::plugin_name_container  current_name : this->plugin_names )
+	 {
+		 this->load_lib(current_name);
+	 }
+ }
+
+ linux_plugin_manager::~linux_plugin_manager()
+ {
+	 this->unload_all_lib();
+ }
+#endif
 
 
 plugin::plugin_manager*  plugin::get_platform_specific_manager()
