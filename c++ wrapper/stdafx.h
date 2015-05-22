b679c7e004bdf44a3793defa0b8e6b8964a1249d
Lukas Rahmn <l.rahmn@gmail.com>
2015-05-23 00:36:53 +0200

    Added prepprocessor macros to remove windows headers on linux

--- c++ wrapper/stdafx.h
+++ c++ wrapper/stdafx.h
@@ -2,7 +2,7 @@
 // or project specific include files that are used frequently, but
 // are changed infrequently
 //
-
+#ifdef WINDOWS
 #pragma once
 
 #include "targetver.h"
@@ -10,7 +10,7 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #include <windows.h>
-
+#endif //WINDOWS
 
 
 // TODO: reference additional headers your program requires here
