254972fad1eb2496b229a26fdc32854ec53a94e0
Lukas Rahmn <l.rahmn@gmail.com>
2015-07-26 13:51:12 +0200

    Added missing file from prev commit

---
+++ core/CalcLib/core/internal_helper.h
@@ -0,0 +1,12 @@
+#include "core/mathNode_interface.h"
+#include "modules/function_helper.h"
+namespace internal_helper
+{
+	template<typename T, typename T2> double forward_fast(typename function_helper::func_type<T, T2>::type func, tree::nodeDataInterface*n)
+	{
+		auto wrapper = tree::nodeDataInterface_wrapper_access(n);
+		
+		return func(function_helper::getData<T>(wrapper->sub1()), function_helper::getData<T2>(wrapper->sub2()));
+	}
+
+}
