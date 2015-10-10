3b283203e4ab2a1f7a0940f61cdd16358b5fa4a4
Lukas Rahmn <l.rahmn@gmail.com>
2015-10-10 23:06:37 +0200

    Updated build system to create an folder with plug-in api headers

---
+++ core/plugins/api_version.h
@@ -0,0 +1,3 @@
+
+#define VERSION_MAJOR "1" //Major version number denotes compability. Two  versions with different Major versions are not compatible
+#define VERSION_MINOR "0" //Minor version number denotes exact subversion of the api. Two versions with same Major version but different minor version is compatible
